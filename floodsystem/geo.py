c2d37b9a01724d5723c6a997f3da411b016e9d57
Yun Meng Khoo <ymk31@cam.ac.uk>
2024-02-05 06:36:20 +0800

    Task 1E done

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -45,3 +45,22 @@ def stations_by_river(stations):
             result[station.river].append(station)
     
     return result
+
+
+#Can we count number of values in key
+#reate list of tuples( river, numberx) wher numberx is number of values
+def rivers_by_station_number(stations, N):
+    """given a list of all station objects (which are station + corresponding details and data), we want to get a list of N rivers and the corresponding number of monitoring stations with the highest nuber of strings"""
+    """From this list, it should be sorted by number of stations from largest to smallest. In the case of ties, all involved rivers are included"""
+    riverdict = stations_by_river(stations)
+    river_number_list = []
+    for station in stations:
+        
+        numberx = len(riverdict[station.river])
+        river_number_list.append((station.river,numberx))
+
+    river_number_list = sorted_by_key(river_number_list, 1).reverse()
+    
+    while river_number_list[N][1] == river_number_list[N-1][1] and N <= len(river_number_list):
+        N += 1
+    return sorted_by_key(river_number_list[:N])
