b887f36160920dd598d93681db2c788a747fb5fb
Yun Meng Khoo <ymk31@cam.ac.uk>
2024-02-05 04:30:12 +0800

    Task 1B done

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,12 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine
+def stations_by_distance(stations, p):
+    station_distance_list = []
+    for station in stations:
+#   
+        distance = haversine(station.coord, p)
+        station_distance_list.append((station,distance))
+        
+    return sorted_by_key(station_distance_list, 1)
